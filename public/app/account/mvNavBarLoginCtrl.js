e3c98b7af9b9da2344a6014c20136d3e252991b8
ishwar6789 <ishwar6789@gmail.com>
2017-01-18 01:53:37 +0530

    refactoring

--- public/app/account/mvNavBarLoginCtrl.js
+++ public/app/account/mvNavBarLoginCtrl.js
@@ -1,12 +1,14 @@
-angular.module('app').controller('myNavBarLoginCtrl',function($scope,$http){
+angular.module('app').controller('myNavBarLoginCtrl',function($scope,$http,mvIdentity,mvNotifier,mvAuth){
+$scope.identity = mvIdentity;
 $scope.signin=function(username,password){
-    $http.post('/login',{username:username,password:password}).then(function(response){
-        if(response.data.success){
-            console.log("loggedin");
-        }else{
-            console.log("incorrect password");
-        }
+mvAuth.authenticateUser(username,password).then(function(success){
+if(success){
+mvNotifier.notify('You have successfully signed in');
 
-    });
+} else{
+mvNotifier.notify('username / Password combination is incorrect');
+}
+});
+    
 }
 });
